ed47048c08bb4a9d3bb4caf6b94381a62db4f1b0
DanielRusinov <daniel.rusinov00@gmail.com>
2017-09-30 13:31:52 -0700

    commiting missing file LULLLLL

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 	 
-	root "application#calc"
+	root "application#quad"
 
  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
