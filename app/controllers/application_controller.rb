212223883b7341e5ad6a23abca1e99822f75b0e3
DanielRusinov <daniel.rusinov00@gmail.com>
2017-09-30 13:23:40 -0700

    I COMMIT QUADRATIC EQUATION,SORRY FOR CAPS

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,23 @@ def calc
 	render html: a + b
 end
 
+def quad
+	a = params[:a].to_i
+	b = params[:b].to_i
+	c = params[:c].to_i
+
+	d = b*b - 4*a*c;
+
+	if( a < 0)
+		puts "Diskriminantata e pod 0 e pedal"
+	elsif( a = 0)
+		x = -b / (2 * a)
+	else x = (-b + Math.sqrt(d)) / (2 * a)
+		y = (-b - Math.sqrt(d)) / (2 * a)
+
+	render html: x, y
+end
+
   protect_from_forgery with: :exception
 end
 
