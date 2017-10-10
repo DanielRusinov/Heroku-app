0f1de982977534c83ffe9dcacfdc66613b9a81e2
DanielRusinov <daniel.rusinov00@gmail.com>
2017-10-10 13:59:42 -0700

    alabala

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,6 +16,62 @@ def sums
 
 end
 
+def filters
+
+	csv = CSV.parse(params[:file].read, converters: :numeric)
+
+	sum = 0;
+	csv.each do |row|
+		if row[2] % 2 != 0
+			sum = sum + row[1]
+		end
+	end
+	
+	render plain: "%.2f" % sum.ceil
 end
+
+def intervals
+
+	csv = CSV.parse(params[:file].read, converters: :numeric)
+	valuesFirstCol = csv.map { |n| n[0] }
+	
+	sum = 0;
+	max_sum = 0
+	
+	if csv.length < 30
+		csv.each do |row|
+			sum = sum + row[0]
+		end
+		max_sum = sum
+	else
+
+		i = 0;
+		j = 0;
+	while i <= csv.lenght - 30 do
+		sum = 0;
+
+		while j <= 30 do
+
+			sum = sum + valuesFirstCol[j + i]
+			 
+		end
+	
+		if sum > max_sum
+			max_sum = sum
+			break;
+		end 
+
+		i++
+	end
+	end
+
+	render plain: "%.2f" % max_sum.ceil
+	
+end
+
+end
+
+
+
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 	 
 	match '/sums', :to => 'application#sums', :via => [:post]
+	match '/filters', :to => 'application#filters', :via => [:post]
+	match '/intervals', :to => 'application#intervals', :via => [:post]
  
 end
