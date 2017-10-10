6f147757a5bdd491a50e5840ace022bf01e06661
DanielRusinov <daniel.rusinov00@gmail.com>
2017-10-10 10:36:01 -0700

    modifing func sum

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,13 +4,14 @@ class ApplicationController < ActionController::Base
 	protect_from_forgery with: :null_sessions
 
 def sums
-	csv_file = (params[:file].read
+	csv_file = (params[:file].read)
 	csv = CSV.parse(csv_file, converters: :numeric)
 	
 	sum = 0
 	csv.each do |row|
   		sum = sum + row[0]
-		sum = sum.ceil
+	
+	render plain: "%.2f" % sum.ceil
 end
 
 end 
