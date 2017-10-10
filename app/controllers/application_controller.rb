4823f83903878fce08b2dcf9a959580c1e33b78f
DanielRusinov <daniel.rusinov00@gmail.com>
2017-10-10 09:57:21 -0700

    commenting the previous funcs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,30 +1,30 @@
 class ApplicationController < ActionController::Base
 	protect_from_forgery with: :exception
 
-def calc
-	a = params[:a].to_i
-	b = params[:b].to_i
-
-	render html: a + b
-end
-
-def quad
-	a = params[:a].to_i
-	b = params[:b].to_i
-	c = params[:c].to_i
-
-	d = b*b - 4*a*c;
-
-	if( d < 0)
-		puts "Diskriminantata e pod 0 e pedal"
-	elsif( d == 0)
-		x = -b / (2 * a)
-	else x = (-b + Math.sqrt(d)) / (2 * a)
-		y = (-b - Math.sqrt(d)) / (2 * a)
-	end
-
-	render html: "x1 = #{x}, x2 = #{y}"  
-end
+#def calc
+#	a = params[:a].to_i
+#	b = params[:b].to_i
+#
+#	render html: a + b
+#end
+#
+#def quad
+#	a = params[:a].to_i
+#	b = params[:b].to_i
+#	c = params[:c].to_i
+#
+#	d = b*b - 4*a*c;
+#
+#	if( d < 0)
+#		puts "Diskriminantata e pod 0 e pedal"
+#	elsif( d == 0)
+#		x = -b / (2 * a)
+#	else x = (-b + Math.sqrt(d)) / (2 * a)
+#		y = (-b - Math.sqrt(d)) / (2 * a)
+#	end
+#
+#	render html: "x1 = #{x}, x2 = #{y}"  
+#end
 
   
 end
