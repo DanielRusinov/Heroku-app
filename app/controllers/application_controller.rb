c171b33fa211a42f4357165ab9ac5e0724c9e6cc
DanielRusinov <daniel.rusinov00@gmail.com>
2017-10-10 12:41:34 -0700

    modifing sums func

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,43 +5,17 @@ class ApplicationController < ActionController::Base
 
 def sums
 	
-	arr = CSV.parse(params["file"].read, converters: :numeric)
-        sum = 0
-        for i in 0..arr.length - 1
-            sum += arr[i][0] 
-        end
+	csv = CSV.parse(params[:file].read, converters: :numeric)
+        
+	sum = 0  
+	csv.each do |row|
+		sum = sum + row[0].to_f
+	end
 
 	render plain: "%.2f" % sum.ceil
 
 end
 
-end 
-
-
-#def calc
-#	a = params[:a].to_i
-#	b = params[:b].to_i
-#
-#	render html: a + b
-#end
-#
-#def quad
-#	a = params[:a].to_i
-#	b = params[:b].to_i
-#	c = params[:c].to_i
-#
-#	d = b*b - 4*a*c;
-#
-#	if( d < 0)
-#		puts "Diskriminantata e pod 0 e pedal"
-#	elsif( d == 0)
-#		x = -b / (2 * a)
-#	else x = (-b + Math.sqrt(d)) / (2 * a)
-#		y = (-b - Math.sqrt(d)) / (2 * a)
-#	end
-#
-#	render html: "x1 = #{x}, x2 = #{y}"  
-#end
-
+end
 
 
